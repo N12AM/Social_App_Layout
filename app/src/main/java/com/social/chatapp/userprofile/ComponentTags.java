e8633c5639f395a81bca07b121028aafdd0e9f54
N12AM <mg.nizam33@gmail.com>
2022-11-11 20:05:18 +0600

    added a incomplete user-profile layout

---
+++ app/src/main/java/com/social/chatapp/userprofile/ComponentTags.java
@@ -0,0 +1,41 @@
+package com.social.chatapp.userprofile;
+
+public class ComponentTags {
+
+    public String [] components =
+            {
+                    "Family Oriented",
+                    "Student",
+                    "Religious",
+                    "Very Religious",
+                    "Slightly Religious",
+                    "Works Out",
+                    "Not Religious",
+                    "Doctor",
+                    "Engineer",
+                    "Pilot",
+                    "Lawyer",
+                    "Hustler",
+                    "Researcher",
+                    "Scientist",
+                    "Foodie",
+                    "Can cook",
+                    "Home Cook",
+                    "Memorized Qur'an",
+                    "Memorizing Qur'an",
+                    "Good Recitation",
+                    "Work Oriented",
+                    "Nerdy",
+                    "Loves Traveling",
+                    "Physical Disability",
+                    "Mental Disability",
+                    "Wants Children",
+                    "Does not want Children",
+                    "Widowed"
+            };
+
+
+    public enum Tags{
+
+    }
+}
